c963262080195387b5ff61cff75f9d9abb84d88a
shashi-kiran-reddy <shashikiranzz99@gmail.com>
2022-08-26 12:00:34 +0530

    django course on urls

---
+++ urls/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("HI")
+def inte(request,parameter):
+    return HttpResponse("HI" + str(parameter))
+def intex(request,parameter,parameter2):
+    return HttpResponse("HI" + parameter + parameter2)
+def custompara(request,parameter):
+    return HttpResponse("12")
+def repath(request,empid):
+    return HttpResponse("repath_successfull!!!")
+def nested(request,page_number):
+    return HttpResponse("done with http response!!!! congoo")
+def test(request,name):
+    return HttpResponse(name)
